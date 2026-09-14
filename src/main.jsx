aee908e927824bd10181c934d7b250140e4798d6
Clement Mukhale <xenobyte012@gmail.com>
2026-09-14 12:43:55 +0200

    path name of the domain

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,9 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/k-k-appliance-repair-site">
+  
+    <BrowserRouter >
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  
 )
